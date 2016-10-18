02f8139f2937d82f4ac785f379a987614d25c54e
olexy <h.lekan@gmail.com>
2016-10-18 16:38:30 +0100

    Added new angular project.

---
+++ templatestore/app/templates/templates.js
@@ -0,0 +1,36 @@
+'use strict';
+//this is a comment.
+var app = angular.module('templateStore.templates', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/templates',{
+            templateUrl: 'templates/templates.html',
+            controller: 'TemplatesCtrl'
+        })
+        .when('/templates/:templateId',{
+            templateUrl: 'templates/templates-details.html',
+            controller: 'TemplateDetailsCtrl'
+        });
+}]);
+
+app.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
+        $http.get('json/templates.json').success(function(data){
+            $scope.templates = data;
+        });
+}]);
+
+app.controller('TemplateDetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', function($scope, $http, $routeParams, $filter){
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').success(function(data){
+        $scope.template = $filter('filter')(data, function(d){
+            return d.id == templateId;
+        })[0];
+
+        $scope.mainImage = $scope.template.images[0].name;
+    });
+
+    $scope.setImage =  function(image){
+        $scope.mainImage =  image.name;
+    };
+}]);
