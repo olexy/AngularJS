78691c07cc08651288cec173f437a8b5418b1823
olexy <h.lekan@gmail.com>
2016-11-16 14:49:42 +0100

    Implementing backend get routes

--- knowledgebase/routes/articles.js
+++ knowledgebase/routes/articles.js
@@ -12,4 +12,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+  Article.getArticleById(req.params.id, function(err, article){
+      if(err){
+          console.log(err);
+      }
+      res.json(article);
+  });
+});
+
+router.get('/category/:category', function(req, res, next) {
+  Article.getArticlesByCategory(req.params.category, function(err, articles){
+      if(err){
+          console.log(err);
+      }
+      res.json(articles);
+  });
+});
+
 module.exports = router;
