c30d2b5c9300ac8b03279be2bee42e4d20de1ef5
olexy <h.lekan@gmail.com>
2016-11-21 11:54:58 +0100

    implementing the final view of the frontend data display

--- knowledgebase/public/controllers/articles.controller.js
+++ knowledgebase/public/controllers/articles.controller.js
@@ -4,4 +4,36 @@ angular.module("kB")
     $http.get('/articles').success(function(data){
         $scope.articles = data;
     });
+}])
+
+.controller('ArticlesCategoryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+    $http.get('/articles/category/' + $routeParams.category).success(function(data){
+        $scope.cat_articles = data;
+        $scope.category = $routeParams.category;
+    });
+}])
+
+.controller('ArticleDetailsCtrl', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location){
+    $http.get('/articles/' + $routeParams.id).success(function(data){
+        $scope.article = data;
+    });
+
+    $scope.removeArticle = function(){
+
+        $http.delete('/articles/'+$routeParams.id)
+
+            .success(function(data){
+                console.log(data);
+            });
+
+
+        // $http.delete('/articles/'+$routeParams.id).success(function(data){
+        //     console.log(data);
+        // });
+
+        $location.path('/articles');
+
+    };
+
+
 }]);
