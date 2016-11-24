930a3d7b14a97008053f099f7cc47f7e2b075f02
olexy <h.lekan@gmail.com>
2016-11-24 15:21:57 +0100

    implementing front end signup by creating signup ejs view, aignup controller and signup module

--- userauth/tasks/pipeline.js
+++ userauth/tasks/pipeline.js
@@ -29,6 +29,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/dependencies/jquery-3.1.1.js',
   'js/dependencies/**/*.js',
   'js//public/signup/SignupMod.js',
   'js//public/signup/SignupCtrl.js',
