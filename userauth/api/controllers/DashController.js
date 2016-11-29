ba760bd86034fc106fd4629928b9a5e63846f251
olexy <h.lekan@gmail.com>
2016-11-29 14:21:58 +0100

    dashboard implementation to list user data

--- userauth/api/controllers/DashController.js
+++ userauth/api/controllers/DashController.js
@@ -8,6 +8,18 @@ module.exports = {
 			console.log('You are logged in');
 			return res.view('dashboard');
 		}
+	},
+
+	getUser: function(req, res){
+		console.log('Running getUser');
+
+		User.findOne({id: req.session.me}, function(err, user){
+			if(err){
+				res.negotiate(err);
+			}
+
+			return res.send(user);
+		})
 	}
 
 };
--- userauth/config/routes.js
+++ userauth/config/routes.js
@@ -36,7 +36,8 @@ module.exports.routes = {
   'GET /signup': {view: 'signup'},
   'GET /dashboard':'DashController.checkUser',
   'POST /signup': 'UserController.signup',
-  'PUT /login': 'UserController.login'
+  'PUT /login': 'UserController.login',
+  'GET /getuser': 'DashController.getUser',
 
   /***************************************************************************
   *                                                                          *
