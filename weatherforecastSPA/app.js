c5bd4757eb2eac9a85d85aa1f8e7544c5f3f4bf4
olexy <h.lekan@gmail.com>
2017-02-09 18:29:02 +0100

    some changes

--- weatherforecastSPA/app.js
+++ weatherforecastSPA/app.js
@@ -3,17 +3,13 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // ROUTES
 weatherApp.config(function ($routeProvider) {
-   
-    $routeProvider
-    
-    .when('/', {
+
+    $routeProvider.when('/', {
         templateUrl: 'pages/home.htm',
-        controller: 'homeController'
-    })
-    
-    .when('/forecast', {
+        controller: 'HomeController'
+    }).when('/forecast', {
         templateUrl: 'pages/forecast.htm',
-        controller: 'forecastController'
+        controller: 'ForecastController'
     })
     
 });
@@ -21,23 +17,36 @@ weatherApp.config(function ($routeProvider) {
 // SERVICES
 weatherApp.service('cityService', function() {
    
-    this.city = "New York, NY";
+    this.city = "London";
     
 });
 
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('HomeController', ['$scope', 'cityService', function($scope, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
-       cityService.city = $scope.city; 
+        cityService.city = $scope.city; 
     });
-    
+       
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('ForecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
     
     $scope.city = cityService.city;
+
+    $scope.weatherAPI = $resource("http://api.apixu.com/v1/forecast.json?key=01d2c236a3e84ccb94f145158170901", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, days: 2 });
+    $scope.weatherResult.then(function(response){
+        console.log(response);
+    }).catch(function(err){
+        console.log(err);
+    })
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
     
 }]);
